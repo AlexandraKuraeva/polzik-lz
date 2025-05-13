9c247c89d3fd44134bc31ab0a530020757e7abab
Alexandra <kuraeva@lz.media>
2025-05-13 23:48:57 +0400

    fix auth.config

--- auth.config.js
+++ auth.config.js
@@ -1,93 +1,75 @@
 import Credentials from "next-auth/providers/credentials";
 import { getUser } from './app/lib/data'
 export const authConfig = {
-
-  providers: [
+	providers: [
 		Credentials({
 			credentials: {
-				email: {  },
-				password: { },
+				email: {},
+				password: {},
 			},
 
-			async authorize(credentials){
-
-        if(!credentials?.email || !credentials?.password){
-          throw new Error("Email и пароль обязательны");
-        }
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials?.password) {
+					throw new Error('Email и пароль обязательны')
+				}
 
 				const user = await getUser(credentials.email)
 
-        if (!user) {
+				if (!user) {
+					throw new Error('Пользователь не найден')
+				}
 
-          throw new Error("Пользователь не найден");
-
-        }
-
-        if (credentials.password !== user.password) {
-
-          throw new Error("Неверный пароль");
-          
-        }
-        console.log(user.id, user.name );
-        return {
-          id: user.id,
-          email: user.email,
-          role: user.role,
-          name: user.name,
-        };
-
+				if (credentials.password !== user.password) {
+					throw new Error('Неверный пароль')
+				}
+				console.log(user.id, user.name)
+				return {
+					id: user.id,
+					email: user.email,
+					role: user.role,
+					name: user.name,
+				}
 			},
-
-     
 		}),
 	],
 
-  pages: {
-    signIn: '/login',
-  },
+	pages: {
+		signIn: '/login',
+	},
 
 	callbacks: {
-     async authorized({ auth, request: { nextUrl } }) {
+		async authorized({ auth, request: { nextUrl } }) {
+			const isLoggedIn = !!auth?.user
 
-       const isLoggedIn = !!auth?.user;
+			const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
 
-       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+			if (isOnDashboard) {
+				if (isLoggedIn) return true
 
-       if (isOnDashboard) {
+				return false
+			}
+			return true
+		},
 
-         if (isLoggedIn) return true;
+		async jwt({ token, user, account }) {
+      console.log("jwt", account);
+			if (user) {
+				token.id = user.id
+				token.role = user.role
+				token.name = user.name
+			}
 
-         return false; 
+			console.log('token', token)
+			return token
+		},
 
-       }
-       return true;
-     },
-
-    async jwt({ token, user }) {
-      console.log(user);
-      if (user) {
-        token.id = user.id;
-        token.role = user.role;
-        token.name = user.name;
-      }
-
-      console.log("token", token);
-      return token;
-    },
-
-    async session({ session, token }) {
-      console.log("session", session);
-      session.user.id = token.id;
-      session.user.role = token.role;
-      session.user.name = token.name;
-      return session;
-    }
-
-
-  },
-  
-  
-	
-  
-
+		async session({ session, token }) {
+			if (token) {
+				session.user.id = token.id
+				session.user.role = token.role
+				session.user.name = token.name
+			}
+			return session
+		},
+	},
 } ;
